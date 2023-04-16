ce6d6c906819de4628421227747d275d576ebbad
Margarita <124924807+221RDB149Margarita@users.noreply.github.com>
2023-04-16 21:00:27 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,30 +1,62 @@
 
-def rabin_karp(pattern, text):
+def read_input():
+    """
+    This function acquires input both from keyboard and file.
+    Use capital "I" (input from keyboard) and capital "F" (input from file) to choose the input type.
 
-    p = len(pattern)
-    t = len(text)
-    result = []
+    After input type choice, read two lines: first line is pattern, and second line is text in which to look for pattern.
+    """
+    input_type = input().strip().upper()
+
+    if input_type == 'F':
+        try:
+            with open("./tests/06") as file:
+                pattern = file.readline().strip()
+                text = file.readline().strip()
+        except FileNotFoundError:
+            print("File not found.")
+            return None
+
+    elif input_type == 'I':
+        pattern = input().strip()
+        text = input().strip()
+
+    else:
+        print("Invalid input type.")
+        return None
+
+    return pattern, text
 
 
-    pattern_hash = sum([ord(pattern[i]) for i in range(p)])
-    text_hash = sum([ord(text[i]) for i in range(p)])
-    
+def get_occurrences(pattern, text):
+    """
+    This function finds the occurrences of pattern in text using Rabin-Karp algorithm.
 
-    for i in range(t - p + 1):
+    Returns a list of indices where pattern is found.
+    """
+    occurrences = []
+    pattern_length = len(pattern)
+    text_length = len(text)
+    pattern_hash = hash(pattern)
 
-        if pattern_hash == text_hash:
-            if pattern == text[i:i+p]:
-                result.append(i)
+    for i in range(text_length - pattern_length + 1):
+        if hash(text[i:i+pattern_length]) == pattern_hash:
+            if text[i:i+pattern_length] == pattern:
+                occurrences.append(i)
 
-        if i < t - p:
-            text_hash = text_hash - ord(text[i]) + ord(text[i+p])
-    
-
-    return result
-
-pattern = input().strip()
-text = input().strip()
+    return occurrences
 
 
-result = rabin_karp(pattern, text)
-print(*result)
+def print_occurrences(output):
+    """
+    This function prints the output of the program.
+    """
+    print(' '.join(map(str, output)))
+
+
+if __name__ == '__main__':
+    input_data = read_input()
+    if input_data:
+        pattern, text = input_data
+        pattern_occurrences = get_occurrences(pattern, text)
+        print_occurrences(pattern_occurrences)
